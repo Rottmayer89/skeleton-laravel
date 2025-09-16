116f2c71ea8d6b5587124b7ad4acbb2201a15956
Rottmayer <rottmayer1989@gmail.com>
2025-09-16 14:50:37 +0330

    Install Tabler Icons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,10 +19,15 @@
 
     <h1 class='text-lime-500 text-xl pt-5 font-bold'>This is Laravel Skeleton, Tailwind, AlpineJS, Pest</h1>
 
+    <p class='text-main-500'>Description</p>
+    <i class="ti ti-heart text-rose-500"></i>
+
 
     <div x-data="{ count: 0 }">
         <button x-on:click="count++"
             class='bg-indigo-500 text-white py-2 px-4 rounded-md cursor-pointer'>Increment</button>
+
+
 
         <span x-text="count" class='ml-5 font-bold'></span>
     </div>
